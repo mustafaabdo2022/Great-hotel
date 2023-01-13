deefe54b8a58bd30cf50780a70b517631b3c452f
Mustafa Abdo <121944633+mustafaabdo2022@users.noreply.github.com>
2023-01-13 04:38:22 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$( function() {
+    $( "#inputCheckIn" ).datepicker();
+    $( "#inputCheckOut" ).datepicker();
+    $('.level-slider').slick({
+      dots: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+
+      
+      
+    });
+  } );
